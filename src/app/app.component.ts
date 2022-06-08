81d01ae50837c74e08053cf748b80ef860e2566d
monica sebastiano <monica.sebastiano@protomgroup.com>
2022-06-08 14:23:11 +0200

    modifica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'progettoAngular';
+
+  cane: any;
+  gatto: any;
+  criceto: any;
+  coniglio: any;
+  pappagallo: any;
+
+
+  funzione(descrizione: any) {
+    this.cane = document.getElementById("Cane");
+    this.gatto = document.getElementById("Gatto");
+    this.criceto = document.getElementById("Criceto");
+    this.coniglio = document.getElementById("Coniglio");
+    this.pappagallo = document.getElementById("Pappagallo");
+    this.criceto.style.display = "none";
+    this.gatto.style.display = "none";
+    this.cane.style.display = "none";
+    this.coniglio.style.display = "none";
+    this.pappagallo.style.display = "none";
+
+
+    if (descrizione == "cane") {
+      
+      if (this.cane.style.display == "none"){
+        this.cane.style.display = "flex";
+      } else{
+        this.cane.style.display = "none";
+
+      }
+      
+      
+      
+    }
+    if (descrizione == "gatto") {
+      if (this.gatto.style.display == "none"){
+        this.gatto.style.display = "flex";
+      } else{
+        this.gatto.style.visibility = "none";
+
+      }
+    }
+    if (descrizione == "criceto") {
+      if (this.criceto.style.display == "none"){
+        this.criceto.style.display = "flex";
+      } else{
+        this.criceto.style.display = "none";
+
+      }
+    }
+
+    if (descrizione == "coniglio") {
+      if (this.coniglio.style.display == "none"){
+        this.coniglio.style.display = "flex";
+      } else{
+        this.coniglio.style.display = "none";
+
+      }
+    }
+
+    if (descrizione == "pappagallo") {
+      if (this.pappagallo.style.display == "none"){
+        this.pappagallo.style.display = "flex";
+      } else{
+        this.pappagallo.style.display = "none";
+
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+  }
+
 }
